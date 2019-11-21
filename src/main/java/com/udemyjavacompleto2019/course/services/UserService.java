687fd0773ad3d20e34e4b2f0e172d5ce919e75c8
rdfarias <rdfarias@zipmail.com.br>
2019-11-20 23:15:34 -0200

    Service layer, component registration

---
+++ src/main/java/com/udemyjavacompleto2019/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.udemyjavacompleto2019.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemyjavacompleto2019.course.entities.User;
+import com.udemyjavacompleto2019.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
